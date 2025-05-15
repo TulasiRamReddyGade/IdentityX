16f148a8661511a3d386cecbbba03907d217d1c8
TulasiRamReddyGade <tulasir287@gmail.com>
2025-05-15 19:05:00 +0530

    ix-2 moving untracked files

---
+++ IdentityX.DataAccess/Migrations/20250515034056_Applications Entity Acive property modified and added unique constraint.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityX.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationsEntityAcivepropertymodifiedandaddeduniqueconstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "Applications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Applications",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_NormalizedName",
+                table: "Applications",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_NormalizedName",
+                table: "Applications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Applications",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+    }
+}
